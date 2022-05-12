a5b7b6049f6de9fd601609a646e6dc8d8dd0c6ea
Picls2 <79704594+Picls2@users.noreply.github.com>
2022-05-12 10:09:23 -0400

    access dom & limit to 2 chars

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import pTable from './periodictable.json' assert {type: 'json'};
 
 let elm1 = document.getElementById("elm1").value;
-
-
+let elm2 = document.getElementById("elm2").value;
+let elm3 = document.getElementById("elm3").value;
+let elm4 = document.getElementById("elm4").value;
+let per1 = document.getElementById("per1").value;
+let per2 = document.getElementById("per2").value;
+let per3 = document.getElementById("per3").value;
+let per4 = document.getElementById("per4").value;
