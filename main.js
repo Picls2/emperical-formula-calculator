32033ec5bbd0506a82e2c681b4299fc9198d6d64
Picls2 <79704594+Picls2@users.noreply.github.com>
2022-05-12 11:33:00 -0400

    convert to percent

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 import pTable from './periodictable.json' assert {type: 'json'};
 
-let elm1 = document.getElementById("elm1").value;
-let elm2 = document.getElementById("elm2").value;
-let elm3 = document.getElementById("elm3").value;
-let elm4 = document.getElementById("elm4").value;
-let per1 = document.getElementById("per1").value;
-let per2 = document.getElementById("per2").value;
-let per3 = document.getElementById("per3").value;
-let per4 = document.getElementById("per4").value;
-let mass = document.getElementById("mass").value;
+let elm1 = document.getElementById("elm1");
+let elm2 = document.getElementById("elm2");
+let elm3 = document.getElementById("elm3");
+let elm4 = document.getElementById("elm4");
+let per1 = document.getElementById("per1");
+let per2 = document.getElementById("per2");
+let per3 = document.getElementById("per3");
+let per4 = document.getElementById("per4");
+let mass = document.getElementById("mass");
+const submit = document.getElementById("submit");
+let mass1;
+let mass2;
+let mass3;
+let mass4;
+
+function convertToPercent(percent, store){
+    store = percent.value * 0.01 * mass.value;
+}
+
+submit.onclick = () => {
+    
+}
