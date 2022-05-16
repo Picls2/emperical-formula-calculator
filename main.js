d5cc0fc296159936885d345b10858666fae1d767
Picls2 <79704594+Picls2@users.noreply.github.com>
2022-05-16 13:21:10 -0400

    test

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ function ratioCalculation(){
     }
 }
 
-submit.onclick = () => {
+submit.ontouchstart = () => {
     ratioCalculation();
      PercentDiv.innerHTML = 
      `${elm1.value != "" ? elm1.value + ": ": ""} ${per1.value > 0 ? per1.value + " * " +  0.01 + " * " + mass.value + " = " + convertToMass(per1) + "g" : ""} <br />
