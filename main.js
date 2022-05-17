e64c7683ea71ba567d60c12e443b51308e94abdf
Picls2 <79704594+Picls2@users.noreply.github.com>
2022-05-16 21:31:49 -0400

    oops

--- main.js
+++ main.js
@@ -86,6 +86,7 @@ submit.onclick = () => {
                 alert("alkidfbdsfbdbfids");
             }
         }
+    }
      PercentDiv.innerHTML = 
      `${elm1.value != "" ? elm1.value + ": ": ""} ${per1.value > 0 ? per1.value + " * " +  0.01 + " * " + mass.value + " = " + convertToMass(per1) + "g" : ""} <br />
      ${elm2.value != "" ? elm2.value  + ": " : ""} ${per2.value > 0 ? per2.value + " * " + 0.01 + " * " + mass.value + " = " + convertToMass(per2) + "g" : ""} <br />
