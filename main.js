ed752430cd2a644eeab1446643be045c9e6821c3
Picls2 <79704594+Picls2@users.noreply.github.com>
2022-05-22 15:56:48 -0400

    Revert "test"
    This reverts commit 76539591b404e2c5ee63993bc6bfe948c348c9f3.

--- main.js
+++ main.js
@@ -76,14 +76,7 @@ function ratioCalculation(){
 }
 
 submit.onclick = () => {
-    function waitForElement(){
-        if(typeof mass !== "undefined"){
-            ratioCalculation();
-        }
-        else{
-            setTimeout(waitForElement, 250);
-        }
-    }
+    ratioCalculation();
      PercentDiv.innerHTML = 
      `${elm1.value != "" ? elm1.value + ": ": ""} ${per1.value > 0 ? per1.value + " * " +  0.01 + " * " + mass.value + " = " + convertToMass(per1) + "g" : ""} <br />
      ${elm2.value != "" ? elm2.value  + ": " : ""} ${per2.value > 0 ? per2.value + " * " + 0.01 + " * " + mass.value + " = " + convertToMass(per2) + "g" : ""} <br />
